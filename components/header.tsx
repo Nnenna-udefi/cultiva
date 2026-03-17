9906f3f602d95b712e76b3ecc2e4566bc26f2670
Nnenna udefi <miriamudefi@gmail.com>
2026-03-17 21:45:05 +0100

    feat: updated nav bar

--- components/header.tsx
+++ components/header.tsx
@@ -4,15 +4,17 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 backdrop-blur w-full border-b border-text bg-primary py-6 px-6 md:px-12">
+    <header className="sticky top-0 z-50 backdrop-blur w-full border-b border-text bg-primary py-2 px-6 md:px-12">
       <nav className="container flex justify-between h-16 max-w-7xl items-center">
-        <div className="ml-6 text-white  px-4 py-2 rounded-md">
-          <h1 className="space-x-2 font-bold font-space-grotesk text-lg italic">
-            Cultiva
+        <div className="ml-6 text-white ">
+          <h1 className="space-x-2 font-bold font-space-grotesk text-lg ">
+            ArchitR
           </h1>
         </div>
         <Link href="#upload-section">
-          <Button size={"lg"}>Analyze your data</Button>
+          <Button variant={"secondary"} size={"lg"}>
+            Analyze your data
+          </Button>
         </Link>
       </nav>
     </header>
