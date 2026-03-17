7b704c90888160dda06bc777c6b47fcce2120141
Nnenna udefi <miriamudefi@gmail.com>
2026-03-17 21:04:10 +0100

    fix: removed dark mode colour

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 backdrop-blur w-full border-b border-text bg-primary">
-      <nav className="container flex h-16 max-w-7xl items-center">
+    <header className="sticky top-0 z-50 backdrop-blur w-full border-b border-text bg-primary py-6 px-6 md:px-12">
+      <nav className="container flex justify-between h-16 max-w-7xl items-center">
         <div className="ml-6 text-white  px-4 py-2 rounded-md">
           <h1 className="space-x-2 font-bold font-space-grotesk text-lg italic">
             Cultiva
           </h1>
         </div>
+        <Link href="#upload-section">
+          <Button size={"lg"}>Analyze your data</Button>
+        </Link>
       </nav>
     </header>
   );
